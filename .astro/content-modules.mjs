0257834a04a3c2d4baf6fc7a34906035b2132e29
Lofa <fajrilef@users.noreply.github.com>
2026-08-08 08:22:53 +0000

    Content Batch 1: 5 artikel keyboard cluster (intro, memilih, switch, vs membrane, hot-swap) + cover SVG

--- .astro/content-modules.mjs
+++ .astro/content-modules.mjs
@@ -1,6 +1,11 @@
 
 export default new Map([
 ["src/content/blog/living/cara-menyimpan-sayuran-agar-tetap-segar-lebih-lama.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fblog%2Fliving%2Fcara-menyimpan-sayuran-agar-tetap-segar-lebih-lama.mdx&astroContentModuleFlag=true")],
+["src/content/blog/tech/apa-itu-hot-swap-keyboard-mechanical.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fblog%2Ftech%2Fapa-itu-hot-swap-keyboard-mechanical.mdx&astroContentModuleFlag=true")],
+["src/content/blog/tech/apa-itu-keyboard-mechanical.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fblog%2Ftech%2Fapa-itu-keyboard-mechanical.mdx&astroContentModuleFlag=true")],
 ["src/content/blog/tech/cara-membersihkan-keyboard-mekanik-yang-benar.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fblog%2Ftech%2Fcara-membersihkan-keyboard-mekanik-yang-benar.mdx&astroContentModuleFlag=true")],
-["src/content/blog/tech/cara-pilih-keyboard-mechanical.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fblog%2Ftech%2Fcara-pilih-keyboard-mechanical.mdx&astroContentModuleFlag=true")]]);
+["src/content/blog/tech/cara-memilih-keyboard-mechanical-pemula.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fblog%2Ftech%2Fcara-memilih-keyboard-mechanical-pemula.mdx&astroContentModuleFlag=true")],
+["src/content/blog/tech/cara-pilih-keyboard-mechanical.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fblog%2Ftech%2Fcara-pilih-keyboard-mechanical.mdx&astroContentModuleFlag=true")],
+["src/content/blog/tech/mechanical-keyboard-vs-membrane.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fblog%2Ftech%2Fmechanical-keyboard-vs-membrane.mdx&astroContentModuleFlag=true")],
+["src/content/blog/tech/perbedaan-red-switch-brown-switch-blue-switch.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fblog%2Ftech%2Fperbedaan-red-switch-brown-switch-blue-switch.mdx&astroContentModuleFlag=true")]]);
 		
