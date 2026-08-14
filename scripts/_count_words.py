ecdd78c7625ba2d9dbc5f053e1f1ca99203d1aac
Fajri Adha <fajrilef@users.noreply.github.com>
2026-08-14 01:36:14 +0000

    chore: remove temp helper scripts

--- scripts/_count_words.py
+++
@@ -1,6 +0,0 @@
-import re
-text = open('src/content/blog/living/berapa-lama-sayuran-bisa-disimpan-di-kulkas.mdx').read()
-body = re.sub(r'^---.*?---', '', text, flags=re.DOTALL)
-body = re.sub(r'[#*_>`\-]', ' ', body)
-words = body.split()
-print('total kata (whitespace):', len(words))
